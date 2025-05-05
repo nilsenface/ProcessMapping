88863e8c66aecbde6919c9b98b53fac3ce18da51
nilsenface <jnilsen94@gmail.com>
2025-05-05 18:52:27 +0200

    Update db_functions.php

--- includes/db_functions.php
+++ includes/db_functions.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/../config.php';
 
 /**
- * Get all data from the database.
+ * Get all data from the database..
  * @return array The complete data structure
  */
 function getAllData() {
