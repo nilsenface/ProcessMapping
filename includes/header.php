7373d5496b7b61394f484459f41ed92375f6d725
nilsenface <jnilsen94@gmail.com>
2025-05-05 18:53:57 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+/**
+ * Get all data from the database..
+ * @return array The complete data structure
+ */
+    <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Business Process Visualization</title>
